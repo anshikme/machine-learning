6147fef1a447381f9e2694184476e56c8242dccc
anshikme <anshikme@rediff.com>
2018-06-26 08:05:07 +0530

    Update pi.py

--- ml/pi.py
+++ ml/pi.py
@@ -1,3 +1,32 @@
-for in data:
-  w.append(is)
-  
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = open("C:\\Users\\science\\Downloads\\data_1d.csv")
+X=[]
+Y=[]
+for i in data:
+    x,y= i.split(",")
+    X.append(float(x))
+    Y.append(float(y))
+
+    
+    
+X = np.array(X)  
+Y = np.array(Y)    
+
+plt.scatter(X,Y)
+plt.show()
+
+#denominator =  ∑(X^2) - x̄*∑(X)
+denominator = X.dot(X)- (X.mean()* X.sum())
+
+a = ((Y.mean()*X.dot(X))- (X.mean()*X.dot(Y)))/denominator
+b = (np.dot(X,Y) - np.sum(X)* np.mean(Y))/denominator
+
+Yhat = []    
+# y = bX +a 
+Yhat= b*X + a
+
+plt.scatter(X,Y)
+plt.plot(X,Yhat)
+plt.show()
